05056d05d1273f79bc9e2f7f2143988ce40f869a
md5crypt <md5crypt@users.noreply.github.com>
2023-06-27 09:07:49 +0200

    v1.0.4: added basic slots support

---
+++ src/Slot.ts
@@ -0,0 +1,29 @@
+import { JSX, StagnateNode } from "./types"
+
+class SlotArray extends Array<StagnateNode> {
+	public readonly name: string
+	constructor (name: string, elements: StagnateNode) {
+		super()
+		this.name = name
+		this.push(elements)
+	}
+}
+
+export function Slot(props: {name: string, children: StagnateNode}): JSX.Element {
+	return new SlotArray(props.name, props.children) as any
+}
+
+Slot.extract = function<T = Record<string, any>>(input: StagnateNode, slots: Record<string, any> = {}) {
+	if (Array.isArray(input)) {
+		for (let i = 0; i < input.length; i += 1) {
+			const item = input[i]
+			if (item instanceof SlotArray) {
+				slots[item.name] = item[0]
+				input[i] = null
+			} else if (Array.isArray(item)) {
+				this.extract(item, slots)
+			}
+		}
+	}
+	return slots as T
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from "./types"
 export * from "./Component"
+export * from "./Slot"
 export { Fragment, createElement } from "./jsx-runtime"
 
 export * as default from "./index"
